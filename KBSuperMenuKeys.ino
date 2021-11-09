ad2d5df0c55f13f894c3f54e67aa991be8d009cf
J MN <jomarno530.git@gmail.com>
2021-11-09 12:27:43 +0100

    Add functionality of Left Super Key

--- KBSuperMenuKeys.ino
+++ KBSuperMenuKeys.ino
@@ -1,9 +1,33 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "Keyboard.h"
 
+const int SuperL_Pin = 9;
+
+bool SuperL_OldState = LOW;
+bool SuperL_NewState = LOW;
+bool SuperL_RisingEdge = LOW;
+bool SuperL_FallingEdge = LOW;
+
+void setup() {
+  pinMode(SuperL_Pin, INPUT);
+
+  Keyboard.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  SuperL_NewState = digitalRead(SuperL_Pin);
+  if (SuperL_NewState == HIGH && SuperL_OldState == LOW) {
+    SuperL_RisingEdge = HIGH;
+  } 
+  else if (SuperL_NewState == LOW && SuperL_OldState == HIGH) {
+    SuperL_FallingEdge = HIGH;
+  } 
+  else {
+    SuperL_RisingEdge = LOW;
+    SuperL_FallingEdge = LOW;
+  }
+  SuperL_OldState = SuperL_NewState;
+  if (SuperL_RisingEdge) {Keyboard.press(KEY_LEFT_GUI);}
+  if (SuperL_FallingEdge) {Keyboard.release(KEY_LEFT_GUI);}
 
 }
